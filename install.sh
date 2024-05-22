ec81b423713002f4b230158f506c5b6b3b9acf49
hellarcore <hellarcoin@gmail.com>
2024-05-22 14:23:43 +0300

    *

--- install.sh
+++ install.sh
@@ -9,46 +9,49 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v0.9.3
+    TAG=v0.9.3
 fi
 
 echo 'VERSION='"${TAG}"'' | sudo -E tee /etc/yiimpoolversion.conf >/dev/null 2>&1
 
 # Clone the Yiimp Install Script repository if it doesn't exist.
-if [ ! -d $HOME/yiimp_install_script ]; then
-	if [ ! -f /usr/bin/git ]; then
-		echo Installing git . . .
-		apt-get -q -q update
-		DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
-		clear
-		echo
+if [ ! -d "$HOME/yiimp_install_script" ]; then
+    if [ ! -f /usr/bin/git ]; then
+        echo Installing git . . .
+        sudo apt-get -q -q update
+        sudo DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
+        clear
+        echo
+    fi
+    
+    echo Downloading Yiimpool Installer ${TAG}. . .
+    git clone \
+        -b ${TAG} --depth 1 \
+        https://github.com/Barzillin/yiimp_install_script \
+        "$HOME"/yiimp_install_script \
+        < /dev/null 2> /dev/null
 
-	fi
-	
-	echo Downloading Yiimpool Installer ${TAG}. . .
-	git clone \
-		-b ${TAG} --depth 1 \
-		https://github.com/Barzillin/yiimp_install_script \
-		"$HOME"/yiimp_install_script \
-		< /dev/null 2> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Failed to clone the repository."
+        exit 1
+    fi
 
-	echo
+    echo
 fi
 
-
-cd $HOME/yiimp_install_script/
+cd "$HOME/yiimp_install_script/" || { echo "Directory $HOME/yiimp_install_script does not exist."; exit 1; }
 
 # Update it.
-sudo chown -R $USER $HOME/yiimp_install_script/.git/
-if [ "${TAG}" != `git describe --tags` ]; then
-	echo Updating Yiimpool Installer to ${TAG} . . .
-	git fetch --depth 1 --force --prune origin tag ${TAG}
-	if ! git checkout -q ${TAG}; then
-		echo "Update failed. Did you modify something in `pwd`?"
-		exit
-	fi
-	echo
+sudo chown -R "$USER" "$HOME/yiimp_install_script/.git/"
+if [ "${TAG}" != "$(git describe --tags)" ]; then
+    echo Updating Yiimpool Installer to ${TAG} . . .
+    git fetch --depth 1 --force --prune origin tag ${TAG}
+    if ! git checkout -q ${TAG}; then
+        echo "Update failed. Did you modify something in $(pwd)?"
+        exit 1
+    fi
+    echo
 fi
 
 # Start setup script.
-bash $HOME/yiimp_install_script/install/start.sh
+bash "$HOME/yiimp_install_script/install/start.sh"
