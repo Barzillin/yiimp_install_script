9b8807a3ea7d3f5db8114c7349d3ed4b14eca7ee
hellarcore <hellarcoin@gmail.com>
2024-05-22 15:00:37 +0300

    *

--- install.sh
+++ install.sh
@@ -9,30 +9,30 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-    TAG=v0.9.3
+	TAG=v0.9.3
 fi
 
 echo 'VERSION='"${TAG}"'' | sudo -E tee /etc/yiimpoolversion.conf >/dev/null 2>&1
 
 # Clone the Yiimp Install Script repository if it doesn't exist.
 if [ ! -d $HOME/yiimp_install_script ]; then
-    if [ ! -f /usr/bin/git ]; then
-        echo Installing git . . .
-        apt-get -q -q update
-        DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
-        clear
-        echo
+	if [ ! -f /usr/bin/git ]; then
+		echo Installing git . . .
+		apt-get -q -q update
+		DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
+		clear
+		echo
 
-    fi
-    
-    echo Downloading Yiimpool Installer ${TAG}. . .
-    git clone \
-        -b ${TAG} --depth 1 \
-        https://github.com/afiniel/yiimp_install_script \
-        "$HOME"/yiimp_install_script \
-        < /dev/null 2> /dev/null
+	fi
+	
+	echo Downloading Yiimpool Installer ${TAG}. . .
+	git clone \
+		-b ${TAG} --depth 1 \
+		https://github.com/afiniel/yiimp_install_script \
+		"$HOME"/yiimp_install_script \
+		< /dev/null 2> /dev/null
 
-    echo
+	echo
 fi
 
 
@@ -41,13 +41,13 @@ cd $HOME/yiimp_install_script/
 # Update it.
 sudo chown -R $USER $HOME/yiimp_install_script/.git/
 if [ "${TAG}" != `git describe --tags` ]; then
-    echo Updating Yiimpool Installer to ${TAG} . . .
-    git fetch --depth 1 --force --prune origin tag ${TAG}
-    if ! git checkout -q ${TAG}; then
-        echo "Update failed. Did you modify something in `pwd`?"
-        exit
-    fi
-    echo
+	echo Updating Yiimpool Installer to ${TAG} . . .
+	git fetch --depth 1 --force --prune origin tag ${TAG}
+	if ! git checkout -q ${TAG}; then
+		echo "Update failed. Did you modify something in `pwd`?"
+		exit
+	fi
+	echo
 fi
 
 # Start setup script.
--- install/preflight.sh
+++ install/preflight.sh
@@ -1,14 +1,42 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
+##################################################################################
+# This is the entry point for configuring the system.                            #
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox   #
+# Updated by Afiniel for yiimpool use...                                         #
+##################################################################################
+if [ "$(lsb_release -d | sed 's/.*:\s*//' | sed 's/20\.04\.[0-9]/20.04/')" == "Ubuntu 20.04 LTS" ]; then
+  DISTRO=20
+  sudo chmod g-w /etc /etc/default /usr
 
-# Check if swap is needed
+elif [ "$(lsb_release -d | sed 's/.*:\s*//' | sed 's/18\.04\.[0-9]/18.04/')" == "Ubuntu 18.04 LTS" ]; then
+  DISTRO=18
+  sudo chmod g-w /etc /etc/default /usr
+
+elif [ "$(lsb_release -d | sed 's/.*:\s*//' | sed 's/16\.04\.[0-9]/16.04/')" == "Ubuntu 16.04 LTS" ]; then
+  DISTRO=16
+  sudo chmod g-w /etc /etc/default /usr
+
+else
+  echo "This script only supports Ubuntu 16.04 LTS, 18.04 LTS, and 20.04 LTS."
+  exit
+fi
+
+# Check if swap is needed.
+
 SWAP_MOUNTED=$(cat /proc/swaps | tail -n+2)
 SWAP_IN_FSTAB=$(grep "swap" /etc/fstab)
 ROOT_IS_BTRFS=$(grep "\/ .*btrfs" /proc/mounts)
 TOTAL_PHYSICAL_MEM=$(head -n 1 /proc/meminfo | awk '{print $2}')
 AVAILABLE_DISK_SPACE=$(df / --output=avail | tail -n 1)
-
-if [ -z "$SWAP_MOUNTED" ] && [ -z "$SWAP_IN_FSTAB" ] && [ ! -e /swapfile ] && [ -z "$ROOT_IS_BTRFS" ] && [ $TOTAL_PHYSICAL_MEM -lt 1536000 ] && [ $AVAILABLE_DISK_SPACE -gt 5242880 ]; then
+if
+  [ -z "$SWAP_MOUNTED" ] &&
+    [ -z "$SWAP_IN_FSTAB" ] &&
+    [ ! -e /swapfile ] &&
+    [ -z "$ROOT_IS_BTRFS" ] &&
+    [ $TOTAL_PHYSICAL_MEM -lt 1536000 ] &&
+    [ $AVAILABLE_DISK_SPACE -gt 5242880 ]
+then
   echo "Adding a swap file to the system..."
 
   # Allocate and activate the swap file. Allocate in 1KB chunks
@@ -22,8 +50,8 @@ if [ -z "$SWAP_MOUNTED" ] && [ -z "$SWAP_IN_FSTAB" ] && [ ! -e /swapfile ] && [ 
   fi
 
   # Check if swap is mounted then activate on boot
-  if swapon -s | grep -q "/swapfile"; then
-    echo "/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab
+  if swapon -s | grep -q "\/swapfile"; then
+    echo "/swapfile  none swap sw 0  0" | sudo tee -a /etc/fstab
   else
     echo "ERROR: Swap allocation failed"
   fi
@@ -34,6 +62,15 @@ if [ "$ARCHITECTURE" != "x86_64" ]; then
   if [ -z "$ARM" ]; then
     echo "Yiimpool Installer only supports x86_64 and will not work on any other architecture, like ARM or 32-bit OS."
     echo "Your architecture is $ARCHITECTURE"
-    exit 1
+    exit
   fi
+fi
+
+# Set STORAGE_USER and STORAGE_ROOT to default values (crypto-data and /home/crypto-data), unless
+# we've already got those values from a previous run.
+if [ -z "$STORAGE_USER" ]; then
+  STORAGE_USER=$([[ -z "$DEFAULT_STORAGE_USER" ]] && echo "crypto-data" || echo "$DEFAULT_STORAGE_USER")
+fi
+if [ -z "$STORAGE_ROOT" ]; then
+  STORAGE_ROOT=$([[ -z "$DEFAULT_STORAGE_ROOT" ]] && echo "/home/$STORAGE_USER" || echo "$DEFAULT_STORAGE_ROOT")
 fi
